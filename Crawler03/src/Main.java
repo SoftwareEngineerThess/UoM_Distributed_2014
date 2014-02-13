1f4cce1dcbfd63e1236d38cabc0a332a7a33d19d
ioaniatr <ioaniatr@gmail.com>
2014-02-13 02:53:38 +0200

    Odigies Gia Petro!! :-D

--- Crawler03/src/Main.java
+++ Crawler03/src/Main.java
@@ -4,6 +4,15 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+/*		PETROOO odigies
+		Enallaktikoi Git Clients anti tou eclipse pou mporeis na xrisimopoiHseis: SmartGit(auon exw egw), kai exw akousei polu kala logia kai gia ton "Aptana". Dialekse opoion 8es 
+		1- GIT PULL: Ferneis ta arxeia sto pc sou me to git. URL "https://github.com/SoftwareEngineerThess/UoM_Distributed_2014.git". workspace tou git o fakelos "UoM_Distributed_2014.git" (dimiourgise ton an den yparxei) mesa sto workspace tou eclipse
+			(to allaksa epidi egine kati periergo kai sbistike to master apo to origin, kai gia na min mpleksw brika neo onoma)
+		2- Apo to eclipse sbineis ta arxeia tou project
+		3- File/import project kai dialegeis to fakelo "workspace\UoM_Distributed_2014.git\Crawler03" pou efere to Git (an exeis tsekarismeno to copy project into workspace ksetsekarise to)
+		4- Onoma project "Crawler03"
+		5- kane mia allagi na dw oti ola einai ok gia na ksekinisoume 
+*/		
 	}
 
 }
